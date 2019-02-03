90de6bce79b71b25e9c1f657e08ca2d87399a24e
VitaliiMelnychukDev <vitaliymelnychukdev@gmail.com>
2019-02-03 16:10:21 +0200

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
-import {FormsModule} from "@angular/forms";
-import {AccountsService} from "./services/account.service";
-import {LoggingService} from "./services/logging.service";
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {UsersComponent} from './users/users.component';
+import {ServersComponent} from './servers/servers.component';
+import {UserComponent} from './users/user/user.component';
+import {EditServerComponent} from './servers/edit-server/edit-server.component';
+import {ServerComponent} from './servers/server/server.component';
+import {ServersService} from './servers/servers.service';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AuthService} from "./auth.service";
+import {AuthGuard} from "./auth-guard.service";
+import {CanDeactiveGuard} from "./servers/edit-server/can-deactive-guard.service";
+import { ErrorPageComponent } from './error-page/error-page.component';
+import {ServerResolver} from "./servers/server/server-resolver.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccountComponent,
-    NewAccountComponent
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent,
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [AccountsService, LoggingService],
+  providers: [
+    ServersService,
+    AuthService,
+    AuthGuard,
+    CanDeactiveGuard,
+    ServerResolver
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
